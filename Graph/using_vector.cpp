220b1e356391c1d4a58f6806df835b3af0aa02d6
Abhishek Singh <singh.abhishek.2017447@gmail.com>
2022-05-30 14:54:44 +0530

    first commit

---
+++ Graph/using_vector.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    vector<int> adj[n+1];
+    /*
+    if edge width is given
+    vector <pair <int, int> > adj[n+1];
+    */
+    for(int i=0;i<m;i++)
+    {
+        int u,v;
+        cin>>u>>v;
+        adj[u].push_back(v);//if directed graph only this use
+        adj[v].push_back(u);
+        /*
+        adj[u].push_back({v,w});
+        adj[v].push_back({u,w});
+        */
+    }
+    return 0;
+}
